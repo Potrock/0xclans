b5f4713d4173c07ae76901188bc256e4361e8a7b
pwblais <pwblais@gmail.com>
2023-02-23 21:24:17 -0500

    Search bar hidden on small

--- components/nav/Navbar.tsx
+++ components/nav/Navbar.tsx
@@ -112,7 +112,7 @@ export const Navbar = () => {
 								</div>
 								<>
 									<form
-										className="flex w-5/12 px-3 py-1 ml-auto mr-2 text-gray-300 placeholder-gray-500 bg-gray-900 border border-gray-600 rounded-md appearance-none focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+										className="hidden w-5/12 px-3 py-1 ml-auto mr-2 text-gray-300 placeholder-gray-500 bg-gray-900 border border-gray-600 rounded-md appearance-none md:flex focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
 										onSubmit={handleSearch}
 									>
 										<input
